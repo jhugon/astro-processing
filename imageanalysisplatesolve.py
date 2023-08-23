68a3597ac0ece46df5533e5fb700219068e7e86a
Justin Hugon <jhugon@users.noreply.github.com>
2023-08-23 11:04:35 -0500

    imageanalysisplatesolve: added find files function

--- imageanalysisplatesolve.py
+++ imageanalysisplatesolve.py
@@ -6,6 +6,23 @@ from astropy.stats import sigma_clipped_stats
 from ccdproc import CCDData
 from photutils.background import Background2D
 
+def findfilesindir(p):
+    if p.is_file():
+        if p.suffix == ".fit":
+            return [p]
+        elif len(p.suffixes) > 1 and p.suffixes[-2] == ".fit" and p.suffix in [".zip",".gz"]:
+            return [p]
+        else:
+            return []
+    elif p.is_dir():
+        result = []
+        for p2 in p.iterdir():
+            tmp = findfilesindir(p2)
+            result += tmp
+        return result
+    else:
+        raise Exception(f"{p} isn't a file or directory")
+
 def main():
     import argparse
     parser = argparse.ArgumentParser(
@@ -22,14 +39,21 @@ def main():
 
     for indir in indirs:
         if not indir.exists():
-            raise IOError(f"{indir} doesn't exist")
+            raise Exception(f"{indir} doesn't exist")
         if not indir.is_dir():
-            raise IOError(f"{indir} isn't a directory")
+            raise Exception(f"{indir} isn't a directory")
         print(indir)
     if not outdir.exists():
         outdir.mkdir(parents=True)
     if not outdir.is_dir():
         raise IOError(f"{outdir} isn't a directory")
+
+    infiles = []
+    for indir in indirs:
+        infiles += findfilesindir(indir)
+    for infile in infiles:
+        print(infile)
+    
         
 
 if __name__ == "__main__":
